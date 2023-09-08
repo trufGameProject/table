604bfdad93e18cd8a2ea5d725b6db9ca3b110dc5
Budi Darmawan <vbudi@us.ibm.com>
2023-09-07 20:31:53 -0500

    initial
    Signed-off-by: Budi Darmawan <vbudi@us.ibm.com>

---
+++ src/test/java/com/vbd/truf/table/TableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbd.truf.table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
